15610cfef890865aa5c37d4d4dd76597b6b3835c
gympohnpimol <gympohnpimol@gmail.com>
2020-03-15 15:01:46 +0700

    complete

--- Code/Visualization_part2.py
+++ Code/Visualization_part2.py
@@ -23,36 +23,37 @@ df["Critic_Count"] = df["Critic_Count"].astype("int64")
 # print(df.info())
 
 """Dataframe Plot"""
-# df[[ x for x in df.columns if "Sale" in x] + 
-#     ["Year_of_Release"]].groupby("Year_of_Release").sum().plot(kind="bar", rot=45)
+df[[ x for x in df.columns if "Sale" in x] + 
+    ["Year_of_Release"]].groupby("Year_of_Release").sum().plot(kind="bar", rot=45)
 
 """Pairplot"""
-# sns.pairplot(df[['Global_Sales', 'Critic_Score', 'Critic_Count', 
-#                  'User_Score', 'User_Count']])
+sns.pairplot(df[['Global_Sales', 'Critic_Score', 'Critic_Count', 
+                 'User_Score', 'User_Count']])
 
 """distplot"""
-# sns.distplot(df["User_Score"])
+sns.distplot(df["User_Score"])
 
 """jointplot"""
-# sns.jointplot(x="Critic_Score", y="User_Score", data=df, kind="scatter")
+sns.jointplot(x="Critic_Score", y="User_Score", data=df, kind="scatter")
 
 """boxplot"""
-# top_platforms = df["Platform"].value_counts().sort_values(ascending=False).head(5).index.values
-# sns.boxplot(y="Platform", x="Critic_Score", data=df[df["Platform"].isin(top_platforms)], orient= "h")
+top_platforms = df["Platform"].value_counts().sort_values(ascending=False).head(5).index.values
+sns.boxplot(y="Platform", x="Critic_Score", data=df[df["Platform"].isin(top_platforms)], orient= "h")
 
 """heatmap"""
-# platform_genre_sales = df.pivot_table( 
-#                         index="Platform",
-#                         columns="Genre",
-#                         values="Global_Sales",
-#                         aggfunc= sum).fillna(0).applymap(float)
-# sns.heatmap(platform_genre_sales, annot=True, fmt=".1f", linewidths=.5)
+platform_genre_sales = df.pivot_table( 
+                        index="Platform",
+                        columns="Genre",
+                        values="Global_Sales",
+                        aggfunc= sum).fillna(0).applymap(float)
+sns.heatmap(platform_genre_sales, annot=True, fmt=".1f", linewidths=.5)
 
 platforms_df = df.groupby('Platform')[['Global_Sales']].sum().join(
     df.groupby('Platform')[['Name']].count()
 )
 platforms_df.columns = ['Global_Sales', 'Number_of_Games']
 platforms_df.sort_values('Global_Sales', ascending=False, inplace=True)
+
 # Create a bar for the global sales
 trace0 = go.Bar(
     x=platforms_df.index,
